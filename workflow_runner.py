6d35bd564a8fcb70decd046e1c3c58c80791b747
Samuel Mishra <sam@takehomes.com>
2024-09-20 14:10:16 -0400

    fix whitespace

--- workflow_runner.py
+++ workflow_runner.py
@@ -1,14 +1,9 @@
-
 from greetings_workflow import greetings_workflow
 from conductor.client.automator.task_handler import TaskHandler
 from conductor.client.configuration.configuration import Configuration
 from conductor.client.workflow.conductor_workflow import ConductorWorkflow
 from conductor.client.workflow.executor.workflow_executor import WorkflowExecutor
 
-def register_workflow(workflow_executor: WorkflowExecutor) -> ConductorWorkflow:
-    workflow = greetings_workflow(workflow_executor=workflow_executor)
-    workflow.register(True)
-    return workflow
 
 if __name__ == '__main__':
     main()
